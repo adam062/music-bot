b8d14006760ba235fc09712a6092309d6e91ccd5
Shuruhatik <72819916+Shuruhatik@users.noreply.github.com>
2022-07-18 15:13:15 +0200

    Update bot.js

--- util/bot.js
+++ util/bot.js
@@ -7,9 +7,6 @@ const { SpotifyPlugin } = require("@distube/spotify");
 const { SoundCloudPlugin } = require("@distube/soundcloud");
 const { YtDlpPlugin } = require("@distube/yt-dlp");
 const { synchronizeSlashCommands, commands } = require("./syncCommands.js");
-//import replit from "quick.replit"
-//const config_replit_db = new replit.Database(process.env["REPLIT_DB_URL"])
-const config_replit_db = new Database({ path: "./token.json", databaseInObject: true })
 const config = new Database({ path: "./config.json", databaseInObject: true })
 const temporary_db = new Database({ path: "./util/temporary_data.json", databaseInObject: true })
 const YouTube = require("youtube-sr").default;
@@ -29,8 +26,7 @@ async function checkVoice(queue, client) {
 module.exports = async function () {
   console.clear()
   const spinner = createSpinner(`Bot processing by \u001b[32;1mShuruhatik#2443\u001b[0m`).start({ color: 'green' })
-  const data_token = await config_replit_db.get(`token`)
-  // const token = data_token.length < 10 ? process.env.token : data_token
+  const data_token = await config.get(`token`)
   const token = data_token
   const client = new Client({
     intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMembers, GatewayIntentBits.GuildMessageReactions, GatewayIntentBits.GuildWebhooks, GatewayIntentBits.GuildIntegrations, GatewayIntentBits.GuildVoiceStates, GatewayIntentBits.GuildMessages],
@@ -48,19 +44,6 @@ module.exports = async function () {
     client.user.setActivity({ name: config.get("status_bot"), type: config.get("status_type") });
     let bot_invite_link = `https://discord.com/api/oauth2/authorize?client_id=${client.user.id}&permissions=8&scope=bot%20applications.commands`
     spinner.success({ text: `Logged in as ${client.user.tag} (${client.user.id})`, color: 'bgGreen' })
-    /* app.get('/',(r, s) => {
-       s.send({message:"Bot by Shuruhatik#2443",youtube_channel:"https://www.youtube.com/ShuruhatikYT"})
-      }).post('/',async(r, s) => {
-        s.send({
-          message:"Bot by Shuruhatik#2443",  youtube_channel:"https://www.youtube.com/ShuruhatikYT"
-        })
-        if(await config_db.has(`uptime`) != true){
-          console.log("\u001b[32m✔ \u001b[0mUptime has been done successfully")
-          await config_db.set(`uptime`,true)
-        }
-      })
-     .get("/invite", (req, res) => res.status(301).redirect(bot_invite_link))
-     .listen(3000)*/
     console.log("\u001b[32m▣ \u001b[38;5;45;1mMusic\u001b[0m Bot By \u001b[38;5;44;1mShuruhatik#2443\u001b[0m ")
     console.log("\u001b[32m▣ \u001b[0m\u001b[0m\u001b[40;1m\u001b[34;1mhttps://" + process.env.REPL_ID + ".id.repl.co/invite\u001b[0m")
     setInterval(async () => {
